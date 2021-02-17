4ac95dd10b78ecc8f9167fa47fdd7e16dda19442
Ricardo <ricaardomartinez4@gmail.com>
2021-02-17 21:29:04 +0100

    actualizacion 17/02/2021

--- orientadoObjetos/conexionBdD.py
+++ orientadoObjetos/conexionBdD.py
@@ -3,18 +3,42 @@
 Conexión con la base de datos.
 """
 import MySQLdb
-import keyring
+
 class conectaMySQL():
+    __bd = None
     
     def __init__(self,username, host, db,passwd):
-        return MySQLdb.connect(host=host, username=username, passwd=passwd, db=db)
-    def insertar(self):
-        sql = "INSERT INTO {}"
-    def modificar(self):
-        pass
-    def listar(self):
-        pass
-    def borrar(self):
-        pass
-    def baseDeDatos(self):
-        pass
+        if not (self.__class__.__bd):
+            self.__class__.__bd = MySQLdb.connect(host=host, username=username, passwd=passwd, db=db)
+    def insertar(self,tabla,diccionario):
+        sql = "INSERT INTO " + tabla + "("
+        for campo in diccionario.keys():
+            sql += campo + ","
+        sql += ") values("
+        for valor in diccionario.values():
+            sql += valor + ","
+        sql = sql[0:-1] + ");"
+        cur = self.__class__.__bd.cursor()
+        cur.execute(sql)
+        cur.commit()
+        cur.close()
+    def modificar(self,tabla,diccionario,pk):
+        sql = "Update " + tabla + " set "
+        for campo,valor in diccionario.items():
+            sql += campo + " = " + valor + ","
+        sql = sql[0:-1] + " where " + tabla + "ID = " + pk + ";"
+        cur = self.__class__.__bd.cursor()
+        cur.execute(sql)
+        cur.commit()
+        cur.close()
+    def listar(self,tabla):
+        sql = "Select * from " + tabla + ";"
+        cur = self.__class__.__bd.cursor()
+        cur.execute(sql)
+        return cur.fetchall()
+    def borrar(self,tabla,pk):
+        sql = "DELETE FROM " + tabla + " WHERE " + tabla + "ID = " + pk + ";" 
+        cur = self.__class__.__bd.cursor()
+        cur.execute(sql)
+        cur.commit()
+        cur.close()
