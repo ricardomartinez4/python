c433d0a9412bb5278d11527a30825fd58ace0331
Ricardo <ricaardomartinez4@gmail.com>
2021-03-01 20:50:58 +0100

    actualizacion 01/03/2021

--- orientadoObjetos/conexionBdD.py
+++ orientadoObjetos/conexionBdD.py
@@ -3,33 +3,33 @@
 Conexión con la base de datos.
 """
 import MySQLdb
-
 class conectaMySQL():
     __bd = None
     
-    def __init__(self,username, host, db,passwd):
+    def __init__(self,username, host, database,passwd):
         if not (self.__class__.__bd):
-            self.__class__.__bd = MySQLdb.connect(host=host, username=username, passwd=passwd, db=db)
+            self.__class__.__bd = MySQLdb.connect(host=host, user=username, passwd=passwd, db=database)
     def insertar(self,tabla,diccionario):
         sql = "INSERT INTO " + tabla + "("
         for campo in diccionario.keys():
             sql += campo + ","
-        sql += ") values("
+        sql = sql[0:-1] + ") values("
         for valor in diccionario.values():
-            sql += valor + ","
+            sql += "'" + valor + "',"
         sql = sql[0:-1] + ");"
         cur = self.__class__.__bd.cursor()
         cur.execute(sql)
-        cur.commit()
         cur.close()
+        self.__class__.__bd.commit()
     def modificar(self,tabla,diccionario,pk):
         sql = "Update " + tabla + " set "
         for campo,valor in diccionario.items():
             sql += campo + " = " + valor + ","
         sql = sql[0:-1] + " where " + tabla + "ID = " + pk + ";"
+        print(sql)
         cur = self.__class__.__bd.cursor()
         cur.execute(sql)
-        cur.commit()
+        self.__class__.__bd.commit()
         cur.close()
     def listar(self,tabla):
         sql = "Select * from " + tabla + ";"
@@ -40,5 +40,62 @@ class conectaMySQL():
         sql = "DELETE FROM " + tabla + " WHERE " + tabla + "ID = " + pk + ";" 
         cur = self.__class__.__bd.cursor()
         cur.execute(sql)
-        cur.commit()
+        self.__class__.__bd.commit()
         cur.close()
+    def conocerCampos(self,tabla):
+        totalCampos = []
+        sql = "SHOW COLUMNS FROM " + tabla + ";"
+        cur = self.__class__.__bd.cursor()
+        cur.execute(sql)
+        todos = cur.fetchall()
+        for row in todos:
+            totalCampos.append(row[0])
+        self.__class__.__bd.commit()
+        cur.close()
+        return totalCampos  
+try:    
+    conexion = conectaMySQL("ricardomartinez4","localhost","prueba2","1234")
+    while(True):
+        print("1 - INSERTAR")
+        print("2 - MODIFICAR")
+        print("3 - LISTAR")
+        print("4 - BORRAR")
+        print("5 - SALIR")
+        opcion = input("Inserte la opcion que desea realizar: ")
+        assert opcion=="1" or opcion=="2" or opcion=="3" or opcion=="4" or opcion=="5", "OPCION NO VÁLIDA"
+        if (opcion=="1"):
+            tabla = input("Tabla que desea introducir valores: ")
+            conocer = conexion.conocerCampos(tabla)
+            print("Campos que tiene {}: {}".format(tabla,conocer))
+            diccionario = {}
+            for x in conocer:
+                diccionario[x] = input("Introduce el valor que desea introducir en {}: ".format(x))
+            insertar1 = conexion.insertar(tabla, diccionario)
+        elif(opcion == "2"):
+            tabla = input("Tabla que desea modificar: ")
+            listado = conexion.listar(tabla)
+            print(listado)
+            pk = input("Identificador de la fila que desea modificar: ")
+            conocer = conexion.conocerCampos(tabla)
+            print("Campos que tiene {}: {}".format(tabla,conocer))
+            campo = input("Campo que desea modificar: ")
+            diccionario = {}
+            for x in conocer:
+                if(x==campo):
+                    diccionario[x] = input("Introduce el valor que desea introducir en {}: ".format(x))
+            modificar1 = conexion.modificar(tabla, diccionario,pk)
+        elif(opcion=="3"):
+            tabla = input("Tabla que desea visualizar: ")
+            print(conexion.listar(tabla))
+        elif(opcion == "4"):
+            tabla = input("Tabla de la que desea borrar valores: ")
+            listado = conexion.listar(tabla)
+            print(listado)
+            pk = input("Identificador del elemento que desea borrar: ")
+            borrar1 = conexion.borrar(tabla, pk)
+            print("Ha sido borrado con éxito!")
+        elif(opcion=="5"):
+            print("Hasta pronto!")
+            break   
+except Exception as e:
+    print(e)
